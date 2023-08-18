983ed7791eea00886611d1712904f22eb67bc13c
adarshbitcot <adarshradhakrishna@bitcot.com>
2023-08-18 10:48:15 +0530

    added gpx static files

--- src/pages/Map/files/index.tsx
+++ src/pages/Map/files/index.tsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React, { Fragment } from "react";
 
-type Props = {}
+type Props = {};
 
 export default function FilePage({}: Props) {
   return (
-    <div>FilePage</div>
-  )
+    <Fragment>
+      <div className="file-container">
+        <GpxFile />
+        <GpxFile />
+        <GpxFile />
+        <GpxFile />
+        <GpxFile />
+      </div>
+    </Fragment>
+  );
 }
+
+const GpxFile = ({ metadata }: any) => {
+  return (
+    <div
+      className="files"
+      key={"smapleData"}
+      // onMouseOver={() => {
+      //   handleFileHover("smapleData");
+      // }}
+    >
+      <p>Sample Data</p>
+    </div>
+  );
+};
--- src/pages/Map/index.tsx
+++ src/pages/Map/index.tsx
@@ -1,12 +1,18 @@
-import React from 'react'
+import React from "react";
+import FilePage from "./files";
 
-type Props = {}
+type Props = {};
 
 export default function MapPage({}: Props) {
   return (
-    <div>MapPage</div>
-  )
+    <div className="main-content">
+      <div>Button sections</div>
+      <div style={{ display: "grid", gridTemplateColumns: "20% 80%" }}>
+        <FilePage  />
+        <div className="" id="map"></div>
+      </div>
+    </div>
+  );
 }
 
-//this MapPage Need to show The Files and Map 
-
+//this MapPage Need to show The Files and Map
