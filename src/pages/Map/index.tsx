99c04216848d32eea40c0813cd2f68e75e244337
adarshbitcot <adarshradhakrishna@bitcot.com>
2023-08-19 12:04:07 +0530

    added filter css

--- src/pages/Map/index.tsx
+++ src/pages/Map/index.tsx
@@ -5,7 +5,33 @@ import GoogleMap from "./gmap";
 export default function MapPage() {
   return (
     <div className="main-content">
-      <div>Button sections</div>
+      <div className="tool-container">
+        <div className="tools">
+          <div className="tool">
+            <label htmlFor="">Tags</label>
+            <select name="" id="">
+              <option>Tags</option>
+            </select>
+          </div>
+          <div className="tool">
+            <label htmlFor="">Kilomete</label>
+            <input type="number" className="no-spinner" name="" id="" />
+          </div>
+          <div className="tool">
+            <label htmlFor="">from</label>
+            <input type="text" name="" id="" />
+          </div>
+          <div className="tool">
+            <label htmlFor="">to</label>
+            <input type="text" name="" id="" />
+          </div>
+          <div className="tool">
+            <label htmlFor="">Time</label>
+            <input type="number" className="no-spinner" name="" id="" />
+          </div>
+          <button className="filter-btn">Apply Filter</button>
+        </div>
+      </div>
       <div style={{ display: "grid", gridTemplateColumns: "20% 80%" }}>
         <FilePage />
         <GoogleMap />
